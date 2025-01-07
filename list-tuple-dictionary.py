67e5c95b73a328f5040dcea345d07b757cccef78
syedmohammedasad365 <2311cs020643@mallareddyuniversity.ac.in>
2025-01-07 15:58:33 +0000

    list-tuple-dictoperations

---
+++ list-tuple-dictionary.py
@@ -0,0 +1,15 @@
+##LIST OPERATIONS
+my_list = [10, 20, 30, 40, 50]
+print("First element:", my_list[0])
+print("Third element:", my_list[2])
+print("Last element:", my_list[-1])
+## TUPLE OPERATIONS
+my_tuple = (1, 2, 3, 4, 5)
+print("Second element:", my_tuple[1])
+print("Fourth element:", my_tuple[3])
+print("Last element:", my_tuple[-1])
+##DICTIONARY OPERATIONS
+my_dict = {"a": "SYED", "b": 200, "c": 300, "d": 400, "e": 500}
+print("Value for key 'a':", my_dict["a"])
+print("Value for key 'c':", my_dict["c"])
+print("Value for key 'e':", my_dict["e"])
