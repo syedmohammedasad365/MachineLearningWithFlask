d84f4fda507094a35c0b52be8257adc3d97a1520
syedmohammedasad365 <2311cs020643@mallareddyuniversity.ac.in>
2025-01-07 15:37:55 +0000

    added datatype.py file

---
+++ datatype.py
@@ -0,0 +1,10 @@
+## This file includes programs to declare variables of different data types like (integer, float, string, and boolean) and print each variable and its type.
+# Declaring variables of different data types
+a = 10
+b = 10.5
+c = "Hello, World!"
+d = True
+print("Integer:", a, "Type:", type(a))
+print("Float:", b, "Type:", type(b))
+print("String:", c, "Type:", type(c))
+print("Boolean:", d, "Type:", type(d))
